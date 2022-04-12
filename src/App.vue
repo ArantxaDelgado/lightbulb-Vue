2945978885b2e85be5b8c30ad771102bf98a3be7
ardelgad <adelgadoporras@gmail.com>
2022-04-12 11:16:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import LightBulb from './components/LightBulb.vue';
+import switchedOn from './components/LightBulb.vue'
+</script>
+
+<template>
+  <h1 v-if="totalLights!=0">¡Enciende todas las luces!</h1>
+  <h1 v-else>¡Todas las luces encendidas!</h1>
+  
+  <!-- escuchar al custom event y ejecutar una función. De momento la función que haga un console.log informando de que se ha encendido. 
+  
+  Cada vez que ejecuta la función decrementa en 1 el número de bombillas que faltan por encender. 
+  
+  Mostrar un mensaje de que todas las bombillas estan encedidas solo cuando totalLight==0-->
+  <main class="bulbs">
+    <light-bulb @switchedOn="ShowMsg" charges="1"></light-bulb>
+    <light-bulb @switchedOn="ShowMsg" charges="1"></light-bulb>
+    <light-bulb @switchedOn="ShowMsg" charges="1"></light-bulb>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      totalLights:3,
+    }
+  },
+
+  methods: {
+    ShowMsg() {
+      this.totalLights--;
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.bulbs {
+  display: flex;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+  }
+
+  #app {
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/LightBulb.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-if="cargas > 0" @click="clicked" class="bulb bulb-off">
+        <p> {{ cargas }} </p>
+    </div>
+    <div v-else class="bulb bulb-on">
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["charges"],
+  emits: ['switchedOn'],
+  data() {
+    return {
+      cargas: this.charges,
+    }
+  },
+  methods: {
+    clicked() {
+        this.cargas--;
+
+        //Cuando cargas == 0 hay que emitir un custom event que le hemos llamado 'switchedOn'.
+        if (this.cargas == 0) {
+            this.$emit('switchedOn')
+        }
+    },
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.bulb {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+}
+
+.bulb-off {
+  width: 265px;
+  height: 400px;
+  background: url("lightoff.png") no-repeat;
+  margin: auto;
+}
+
+.bulb-on {
+  width: 265px;
+  height: 400px;
+  background: url("lighton.png") no-repeat;
+  margin: auto;
+}
+</style>
